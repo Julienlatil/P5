17db2c04d8254601f10b5d6695c921dbdabea6fb
Julienlatil <77979338+Julienlatil@users.noreply.github.com>
2021-06-07 18:34:48 +0200

    Add files via upload

---
+++ frontend/js/confirm.js
@@ -0,0 +1,27 @@
+///// ///// ///// ///// PAGE CONFIRMATION ////// ///// ///// /////
+
+// Confirmation de la commande
+function addConfirmationOrder() {
+    const confirmationId = localStorage.getItem("orderConfirmationId");
+    const messageConfirmation = document.getElementById("orderId");
+    messageConfirmation.innerHTML = "Merci pour votre commande n° " + confirmationId;
+    const totalPrice = localStorage.getItem("totalOrder");
+    const confirmationPrice = document.getElementById("total-price");
+    confirmationPrice.innerHTML = "Prix total : " + totalPrice + " €";
+}
+
+// Vider le panier, le prix total et l'id de commande
+function resetOrder() {
+    buttonHome = document.getElementById('btn-confirmation');
+    buttonHome.addEventListener('click', function () {
+        localStorage.removeItem("orderConfirmationId");
+        localStorage.removeItem('basketContent');
+        localStorage.removeItem('totalOrder');
+        window.location.href = "../../index.html";
+    })
+}
+
+
+////////////////////////////////////APPEL DES FONCTIONS/////////////////////////////////////////////////
+addConfirmationOrder()
+resetOrder()
